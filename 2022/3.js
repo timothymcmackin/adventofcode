acf1135c9b41e4c8332d2cefc64870ca42c26d4d
Tim McMackin <tmcmackin@shutterstock.com>
2022-12-03 21:37:38 -0500

    2022 day 3 part 2

--- 2022/3.js
+++ 2022/3.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 
 const testInputString = `vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -32,10 +33,38 @@ const scoreDuplicates = (sacks) => {
 }
 
 
-// const testInput = testInputString.split('\n').map(processInput);
-// const testDuplicateScore = scoreDuplicates(testInput);
-// console.log(testDuplicateScore);
+const testInput = testInputString.split('\n').map(processInput);
+const testDuplicateScore = scoreDuplicates(testInput);
+console.log(testDuplicateScore);
 
-const inputSacks = fs.readFileSync(path.resolve(__dirname, './inputs/3.txt'), 'utf8').split('\n').map(processInput);;
+const inputSacks = fs.readFileSync(path.resolve(__dirname, './inputs/3.txt'), 'utf8').split('\n').map(processInput);
 const inputDuplicateScore = scoreDuplicates(inputSacks);
 console.log(inputDuplicateScore); //8243
+
+// part 2
+const inputSacksPart2 = fs.readFileSync(path.resolve(__dirname, './inputs/3.txt'), 'utf8').split('\n');
+const testInputSacksPart2 = testInputString.split('\n');
+
+const getDuplicatesInGroups = (sacks) => {
+  let dupes = [];
+  while (sacks.length > 0) {
+    const a = sacks.shift().split('');
+    const b = sacks.shift().split('');
+    const c = sacks.shift().split('');
+    // find the character in all 3
+    const inAllThree = _.uniq(a.filter((char) => b.includes(char) && c.includes(char)));
+    if (inAllThree.length === 1) {
+      dupes.push(inAllThree);
+    } else {
+      console.error('found more than one match');
+    }
+  }
+  return dupes;
+};
+
+const testDupes = getDuplicatesInGroups(testInputSacksPart2);
+console.log(testDupes)
+
+const inputDupes = getDuplicatesInGroups(inputSacksPart2);
+const inputDupesScores = inputDupes.reduce((sum, char) => letterScores[char] ? sum + letterScores[char] : sum, 0);
+console.log(inputDupesScores);
