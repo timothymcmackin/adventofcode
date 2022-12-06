48a62e19ef0e14a70f533efa4d26687fe3fee59f
Tim McMackin <tmcmackin@shutterstock.com>
2022-12-06 09:28:58 -0500

    2018 day 18 part 2; not sure how to do this cleanly

--- 2018/18.js
+++ 2018/18.js
@@ -66,9 +66,10 @@ const getNewAcre = (field, x, y) => {
   return currentAcre;
 }
 
-const runTime = (passedField, time) => {
+const runTime = (passedField, time, scoreAfterEach = false) => {
   let field = JSON.parse(JSON.stringify(passedField));
   let newField = JSON.parse(JSON.stringify(passedField));
+  let prevScore = getScoreCount(field);
   for (let ticker = 1; ticker <= time; ticker++) {
     for (let y = 0; y < field.length; y++) {
       for (let x = 0; x < field[0].length; x++) {
@@ -76,7 +77,12 @@ const runTime = (passedField, time) => {
       }
     }
     field = JSON.parse(JSON.stringify(newField));
-    printField(field, ticker);
+    // printField(field, ticker);
+    if (scoreAfterEach) {
+      const currentScore = getScoreCount(field);
+      console.log('time: ', ticker, ', score:', currentScore, 'for a change of ', prevScore - currentScore, ' with mod', ticker % 28);
+      prevScore = currentScore;
+    }
   }
   return field;
 };
@@ -92,13 +98,70 @@ const getScoreCount = (field) => {
   return treeCount * lumberyardCount;
 }
 
-const testField = parseInput(testFieldStr);
-printField(testField, 0);
-const testFieldAfter10 = runTime(testField, 10);
-if (getScoreCount(testFieldAfter10) !== 1147) {
-  console.error('part 1 test failed. Should be 1147, but got ', getScoreCount(testFieldAfter10));
-}
+// const testField = parseInput(testFieldStr);
+// printField(testField, 0);
+// const testFieldAfter10 = runTime(testField, 10);
+// if (getScoreCount(testFieldAfter10) !== 1147) {
+//   console.error('part 1 test failed. Should be 1147, but got ', getScoreCount(testFieldAfter10));
+// }
 
 const inputField = parseInput(inputStr);
-const inputFieldAfter10 = runTime(inputField, 10);
-console.log('Part 1:', getScoreCount(inputFieldAfter10));
+// const inputFieldAfter10 = runTime(inputField, 10);
+// console.log('Part 1:', getScoreCount(inputFieldAfter10));
+
+console.log(getScoreCount(runTime(inputField, 10000, true)));
+// Pattern starts at +6517 at time 10000
+const deltas = [
+  -3,
+  3600,
+  3320,
+  6517,
+  4355,
+  4216,
+  1136,
+  3172,
+  -426,
+  1140,
+  -2133,
+  -241,
+  -2721,
+  -476,
+  -435,
+  -1152,
+  -1109,
+  -2271,
+  -2281,
+  -2457,
+  -1794,
+  -3692,
+  -2758,
+  -3474,
+  -453,
+  453
+  -606,
+  573,
+];
+
+// Pattern repeats every 28
+// at time 10000 the score is 201123 and that was a change of 6517
+// there's off by one in here somewhere
+
+const simulateFutureScore = (startingScore, startingTime, endTime) => {
+  let score = startingScore;
+  let deltaCycle = JSON.parse(JSON.stringify(deltas));
+  for (let time = startingTime; time <= endTime; time++) {
+    const currentDelta = deltaCycle.shift();
+    score += currentDelta;
+    deltaCycle.push(currentDelta);
+  }
+  return score;
+};
+// console.log(simulateFutureScore(201123, 10000, 1000000000));
+// 217612 too high
+
+// Try simulating to 20000
+// brute force solution to 20000 is 188244
+// console.log(simulateFutureScore(201123, 10000, 20000));
+// console.log(1000000000 % 28) // 20
+// time:  9988 , score: 202806 for a change of  -2457  with mod 20
+// Part 2: 202806
