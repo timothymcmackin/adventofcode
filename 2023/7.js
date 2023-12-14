7e93f64cfbed2119ff8ab6211c5f34f1d5b9ae74
Tim McMackin <timothymcmackin@yahoo.com>
2023-12-14 16:42:57 -0500

    oops

--- 2023/7.js
+++ 2023/7.js
@@ -96,7 +96,7 @@ const handSortFunction = (aHand, bHand) => {
   const a = getType(aHand.hand);
   const b = getType(bHand.hand);
   if (a < b) return -1;
-  if (b > a) return 1;
+  if (a > b) return 1;
   // If two hands have the same type, a second ordering rule takes effect. Start by comparing the first card in each hand.
   let result = 0;
   let aArray = aHand.hand.split('');
@@ -108,7 +108,6 @@ const handSortFunction = (aHand, bHand) => {
   }
   return result;
 }
-
 const testTyping = () => {
   [
     ["32T3K", 6],
@@ -174,5 +173,100 @@ const getTotalPart1 = (inputHands) => {
 const testTotalPart1 = getTotalPart1(testInput);
 if (testTotalPart1 !== 6440) {
   console.log('Test part 1 failed; got', testTotalPart1);
+} else {
+  console.log('Part 1:', getTotalPart1(input));
 }
-// console.log('Part 1:', getTotalPart1(input));
+
+
+// Part 2: Js are jokers
+const ranks2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'];
+
+const rankSortFunction2 = (aChar, bChar) => {
+  const a = ranks2.indexOf(aChar);
+  const b = ranks2.indexOf(bChar);
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+}
+
+const getType2 = (handString) => {
+  const hand = handString.split('');
+  const groups = getGroups(hand);
+
+  // Part 2: put Js with the largest group of non-Js
+  if (groups.J && groups.J > 0) {
+    const numJs = groups.J;
+    delete groups.J;
+    const keys = Object.keys(groups);
+    const keyOfLargest = keys.reduce((prevLargest, oneKey) => {
+      if (groups[prevLargest] < groups[oneKey]) {
+        return oneKey;
+      }
+      return prevLargest;
+    });
+  }
+
+  const numGroups = Object.keys(groups).length;
+
+  if (numGroups === 1) return 1; // Five of a kind
+
+  if (numGroups === 5) return 7; // High card
+  if (numGroups === 4) return 6; // One pair
+
+  const keys = Object.keys(groups);
+  if (numGroups == 3) {
+    // Two pair or three of a kind
+    // If there's a 3, it's three of a kind
+    let result;
+    keys.forEach((key) => {
+      if (groups[key] === 3) {
+        // Three of a kind
+        result = 4;
+      }
+    });
+    return result || 5;
+  }
+
+  if (numGroups === 2) {
+    // Four of a kind or full house
+    // If there's a 4, it's 4 of a kind
+    let result;
+    keys.forEach((key) => {
+      if (groups[key] === 4) {
+        // Four of a kind
+        result = 2;
+      }
+    });
+    return result || 3;
+  }
+}
+
+const handSortFunction2 = (aHand, bHand) => {
+  const a = getType2(aHand.hand);
+  const b = getType2(bHand.hand);
+  if (a < b) return -1;
+  if (a > b) return 1;
+  // If two hands have the same type, a second ordering rule takes effect. Start by comparing the first card in each hand.
+  let result = 0;
+  let aArray = aHand.hand.split('');
+  let bArray = bHand.hand.split('');
+  while (result === 0 && aArray.length > 0) {
+    const charA = aArray.shift();
+    const charB = bArray.shift();
+    result = rankSortFunction2(charA, charB);
+  }
+  return result;
+}
+
+const getTotalPart2 = (inputHands) => {
+  const sortedHands = inputHands.sort(handSortFunction2).reverse();
+  return sortedHands.reduce((sum, { bid }, index) =>
+    sum + (bid * (index + 1))
+  , 0);
+}
+const testTotalPart2 = getTotalPart2(testInput);
+if (testTotalPart2 !== 5905) {
+  console.log('Test part 2 failed; got', testTotalPart2);
+} else {
+  console.log('Part 2:', getTotalPart2(input));
+}
