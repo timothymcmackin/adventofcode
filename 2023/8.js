1a41f03853eaf2eefa8fb63279f5c11b4b71902b
Tim McMackin <timothymcmackin@yahoo.com>
2023-12-14 20:17:23 -0500

    2023 day 8 part 2 wip

--- 2023/8.js
+++ 2023/8.js
@@ -79,3 +79,54 @@ if (test2 !== 6) {
 
 const inputResult = getIterationsToZZZ(processInput(inputStr));
 console.log("Part 1:", inputResult);
+
+// Part 1: take all of the same paths starting from a until all instances are on z
+
+const testInputStrPart2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+const getIterationsToZPart2 = ({ directions, paths }) => {
+  // Start at all paths ending with A
+  let currentLocations = paths
+    .filter(({ source }) => source.endsWith('A'))
+    .map(({ source }) => source);
+  let counter = 0;
+
+  while (counter < 1000000) {
+    let steps = JSON.parse(JSON.stringify(directions));
+    while (steps.length > 0) {
+      counter++;
+      const currentStep = steps.shift();
+
+      // Remove duplicates
+      currentLocations = [...new Set(currentLocations)];
+
+      let newLocations = currentLocations.map((oneLocation) => {
+        const currentPath = paths.find(({ source }) => source === oneLocation);
+        return currentPath[currentStep];
+      });
+
+      if (newLocations.every((oneLocation) => oneLocation.endsWith('Z'))) {
+        return counter;
+      }
+      currentLocations = newLocations;
+    }
+  }
+  return counter;
+}
+
+const testPart2 = getIterationsToZPart2(processInput(testInputStrPart2));
+if (testPart2 !== 6) {
+  console.log("TestPart2 failed; should be 6 but got ", testPart2);
+} else {
+  console.log("Part 2: ", getIterationsToZPart2(processInput(inputStr)));
+}
+// 1000142 too low
