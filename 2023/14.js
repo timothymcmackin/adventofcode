bc46946ecc75b6708f7052d6d5525569e3ba75d4
Tim McMackin <timothymcmackin@yahoo.com>
2024-01-01 15:47:40 -0500

    2023 day 14 part 1

--- 2023/14.js
+++ 2023/14.js
@@ -25,3 +25,53 @@ const calculateWeight = (passedGrid) => {
 if (calculateWeight(calculateWeightTestGrid) !== 136) {
   console.log('calculateWeight test failed');
 }
+
+const testInput = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+  .split('\n')
+  .map((oneLine) => oneLine.split(''));
+
+// Start at the top row and if there is an open spot, move down till you hit a # or if you find a O, put it up.
+const rollUp = (passedGrid) => {
+  let grid = JSON.parse(JSON.stringify(passedGrid));
+  for (let currentRow = 0; currentRow < grid.length; currentRow++) {
+    for (let currentCol = 0; currentCol < grid[0].length; currentCol++) {
+      const targetChar = grid[currentRow][currentCol];
+      if (targetChar === '.') {
+        // Search down and see if you can find a O
+        let checkRow = currentRow + 1;
+        let hitBlock = false;
+        while (checkRow < grid.length && !hitBlock) {
+          let checkChar = grid[checkRow][currentCol];
+          if (checkChar === '#') {
+            hitBlock = true;
+          } else if (checkChar === 'O') {
+            // Move this O up
+            grid[checkRow][currentCol] = '.';
+            grid[currentRow][currentCol] = 'O';
+            hitBlock = true;
+          }
+          checkRow++;
+        }
+      }
+    }
+  }
+  return grid;
+}
+if (calculateWeight(rollUp(testInput)) !== 136) {
+  console.log('rollUp test failed');
+}
+
+const { inputStr } = require('./input/14');
+
+const input = inputStr.split('\n').map((oneLine) => oneLine.split(''));
+
+console.log('Part 1: ', calculateWeight(rollUp(input)));
