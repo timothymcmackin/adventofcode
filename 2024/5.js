c0027a32502294e70b117fd3d023ade8377924a5
Tim McMackin <timothymcmackin@yahoo.com>
2024-12-05 16:24:50 -0500

    2024 day 5 part 2

--- 2024/5.js
+++ 2024/5.js
@@ -85,9 +85,69 @@ const partOne = (inputString) => {
   return booksThatPass.reduce((total, oneBook) => total + getBookScore(oneBook), 0);
 }
 
+// Put a failed book in order
+const correctBook = (passedBook, requirements) => {
+  const remainingInBook = JSON.parse(JSON.stringify(passedBook));
+  let correctedBook = [];
+  // Iterate to see which should go next
+  // Should I do this backwards?
+  // The first item should have no requirements
+  // Or I could add them one at a time and find their places as dependencies get met
+
+  // Find a number that doesn't have any of the others in its dependencies
+  while (remainingInBook.length > 0) {
+    let currentNumber = remainingInBook.shift();
+    // If the number has no missing dependencies it can go in
+    const hasNoRequirements = !requirements[currentNumber];
+    // If no other number has to be before it, it can go in
+    const noOtherNumberDependsOnIt = remainingInBook.filter((n) => !requirements[n]?.includes(currentNumber)).length === 0;
+    // Otherwise add it back to the list, risking infinite loop
+    if (noOtherNumberDependsOnIt || hasNoRequirements) {
+      correctedBook.push(currentNumber);
+    } else {
+      remainingInBook.push(currentNumber);
+    }
+  }
+  return correctedBook;
+}
+
+// Get the books that fail, put them in order, and get their scores
+const partTwo = (inputString) => {
+  // Numbers with the pages that must come before them
+  let requirements = {};
+  let currentLine = '*';
+  let lines = inputString.split('\n');
+
+  while (currentLine !== '') {
+    currentLine = lines.shift();
+    if (currentLine === '') {
+      break;
+    }
+    let [a, b] = currentLine.split('|').map((s) => parseInt(s));
+    if (!requirements[b]) {
+      requirements[b] = [];
+    }
+    if (!requirements[b].includes(a)) {
+      requirements[b].push(a);
+    }
+  }
+
+  const books = lines.map((oneLine) => oneLine.split(',').map((s) => parseInt(s)));
+  const booksThatFail = books.filter((oneBook) => !checkBook(oneBook, requirements));
+  const booksThatFailInCorrectOrder = booksThatFail.map((book) => correctBook(book, requirements));
+  return booksThatFailInCorrectOrder.reduce((total, oneBook) => total + getBookScore(oneBook), 0);
+}
+
 const part1TestScore = partOne(testInputString);
 if (part1TestScore != 143) {
   console.log('Part 1 test failed, got ', part1TestScore);
 } else {
   console.log('Part 1:', partOne(inputString));
+}
+
+const part2TestScore = partTwo(testInputString);
+if (part2TestScore != 123) {
+  console.log('Part 2 test failed, got ', part2TestScore);
+} else {
+  console.log('Part 2:', partTwo(inputString));
 }
