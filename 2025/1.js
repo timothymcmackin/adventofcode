6a48e7778d245b60d224aafc28aaa942b981ea3d
Tim McMackin <tim.mcmackin@trili.tech>
2026-01-25 12:10:47 -0500

    2025 day 1 part 2

--- 2025/1.js
+++ 2025/1.js
@@ -15,6 +15,7 @@ R14
 L82`;
 
 const testCaseAnswer = 3;
+const testCaseAnswerPart2 = 6;
 
 // Count the number of times that the 100-number dial rests on 0 given the rotations listed
 
@@ -31,14 +32,66 @@ const countZeroes = (inputString) => {
     const newCount = newPosition % 100 === 0 ? prevCount + 1 : prevCount;
     return {
       prevCount: newCount,
-      prevPosition: newPosition
+      prevPosition: newPosition,
     }
   }, {prevCount: 0, prevPosition: startPosition});
   return result.prevCount;
 }
+
+// Part 2: count every time the dial passes 0 or lands on zero
+
+const countTouchingZero = (inputString) => {
+  const startPosition = 50;
+  const instructions = inputString.split('\n');
+  const result = instructions.reduce(({ prevCount, prevPosition }, oneInstruction) => {
+    const direction = oneInstruction.substring(0, 1);
+    let moveNumber = Number(oneInstruction.substring(1));
+    let newCount = prevCount;
+    let newPosition;
+    // Get it down below 100
+    newCount += Math.floor(moveNumber/100);
+    if (Math.floor(moveNumber/100) > 1) {
+      console.log('Count', Math.floor(moveNumber/100));
+    }
+    const remainingMoveUnder100 = moveNumber % 100;
+    if (direction === 'R') {
+      newPosition = prevPosition + remainingMoveUnder100;
+      if (newPosition >= 100) {
+        newPosition -= 100;
+        newCount++;
+      }
+    } else {
+      newPosition = prevPosition - remainingMoveUnder100;
+      if (newPosition < 0) {
+        newPosition += 100;
+        if (prevPosition !== 0) {
+          newCount++;
+        }
+      } else if (newPosition === 0) {
+        newCount++;
+      }
+    }
+    // console.log(`THe dial is rotated ${oneInstruction} to point at ${newPosition}`);
+    return {
+      prevCount: newCount,
+      prevPosition: newPosition,
+    }
+  }, {prevCount: 0, prevPosition: startPosition});
+  return result.prevCount;
+}
+
+
 if (countZeroes(testCase) === testCaseAnswer) {
   console.log('Test case OK');
   console.log('Part 1:', countZeroes(rawInput));
 } else {
-  console.error('Test case failed');
+  console.error('Test case 1 failed');
+}
+
+if (countTouchingZero(testCase) === testCaseAnswerPart2) {
+  console.log('Test case OK');
+  console.log('Part 2:', countTouchingZero(rawInput));
+  // 5776 too low
+} else {
+  console.error('Test case 2 failed');
 }
